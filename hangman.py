63197b795f151f44cae0bedca1ab6ff60ee4b2ca
Bertie Ellison-Wright <bellisonwright@gmail.com>
2021-02-22 13:41:00 +0000

    Cleaned up, created dict for outputs

--- hangman.py
+++ hangman.py
@@ -16,6 +16,15 @@ class Hangman():
     '''
     Game of hangman.
     '''
+    outputs_dict = {
+        "correct": "Well done, \'{letter}\' is correct!",
+        "won": "Congratulations, you win!\nThe word was \'{word}\'.",
+        "incorrect": "Unlucky, \'{letter}\' is incorrect...",
+        "lost": "Unlucky, \'{letter}\' is incorrect... and you lose.\nThe word was \'{word}\'.",
+        "already lost": "You already lost...",
+        "already tried": "\'{letter}\' has already been guessed. Please choose another letter.",
+        "not a letter": "\'{letter}\' is not a letter. Please try again."
+    }
     def __init__(self, dictionary_array, print_outputs=True, pictures=None):
         '''
         :param dictionary_array: dictionary, scraped from Oxford Dictionary of English
@@ -24,7 +33,7 @@ class Hangman():
         '''
         self.dictionary = dictionary_array
         self.print_outputs = print_outputs
-        self.pictures = pictures if pictures else ['\n\n\n\n\n\n\n         ', '\n\n       \n       \n       \n       \n       \n=========', '\n       \n      |\n      |\n      |\n      |\n      |\n=========', '\n  +---+\n      |\n      |\n      |\n      |\n      |\n=========', '\n  +---+\n     \\|\n      |\n      |\n      |\n      |\n=========', '\n  +---+\n  |  \\|\n      |\n      |\n      |\n      |\n=========', '\n  +---+\n  |  \\|\n  O   |\n      |\n      |\n      |\n=========', '\n  +---+\n  |  \\|\n  O   |\n  |   |\n      |\n      |\n=========', '\n  +---+\n  |  \\|\n  O   |\n /|   |\n      |\n      |\n=========', '\n  +---+\n  |  \\|\n  O   |\n /|\\  |\n      |\n      |\n=========', '\n  +---+\n  |  \\|\n  O   |\n /|\\  |\n /    |\n      |\n=========', '\n  +---+\n  |  \\|\n  O   |\n /|\\  |\n / \\  |\n      |\n=========']
+        self.pictures = pictures if pictures else ['\n\n\n\n\n\n\n         ', '\n\n       \n       \n       \n       \n       \n=========', '\n       \n      |\n      |\n      |\n      |\n      |\n=========', '\n  +---+\n      |\n      |\n      |\n      |\n      |\n=========', '\n  +---+\n     \\|\n      |\n      |\n      |\n      |\n=========', '\n  +---+\n  |  \\|\n      |\n      |\n      |\n      |\n=========', '\n  +---+\n  |  \\|\n  O   |\n      |\n      |\n      |\n=========', '\n  +---+\n  |  \\|\n  O   |\n  |   |\n      |\n      |\n=========', '\n  +---+\n  |  \\|\n  O   |\n /|   |\n      |\n      |\n=========', '\n  +---+\n  |  \\|\n  O   |\n /|\\  |\n      |\n      |\n=========', '\n  +---+\n  |  \\|\n  O   |\n /|\\  |\n /    |\n      |\n=========', '                 ______\n  +---+         / ____/___ _____ ___  ___ \n  |  \|        / / __/ __ `/ __ `__ \/ _ \ \n  O   |       / /_/ / /_/ / / / / / /  __/\n /|\  |       \____/\__,_/_/ /_/ /_/\___/ \n / \  |             / __ \_   _____  _____\n      |            / / / / | / / _ \/ ___/\n=========         / /_/ /| |/ /  __/ /\n                  \____/ |___/\___/_/']
 
         self.incorrect_letters = []
         self.guessed_letters = []
@@ -39,14 +48,18 @@ class Hangman():
         self.word_to_guess = word_to_guess.lower()
         self.blanks = ["_" for i in range(len(self.word_to_guess))]
 
-    def print_update(self):
-        if not self.has_lost_game:
-            print(self.pictures[len(self.incorrect_letters)])
-        else:
-            print(self.pictures[-1])
-        print(" ".join(self.blanks))
-        if len(self.incorrect_letters) > 0:
-            print("Incorrect guesses:", " ".join([i.upper() for i in self.incorrect_letters]))
+    def print_update(self, letter, message, valid_guess=True):
+        if self.print_outputs:
+            print("\n" + "".join(["━" for i in range(42)]) + "\n")
+            print(self.outputs_dict[message].format(letter=letter.upper(), word=self.word_to_guess.upper()))
+            if valid_guess:
+                if not self.has_lost_game:
+                    print(self.pictures[len(self.incorrect_letters)])
+                else:
+                    print(self.pictures[-1])
+                print(" ".join(self.blanks))
+                if len(self.incorrect_letters) > 0:
+                    print("Incorrect guesses:", " ".join([i.upper() for i in self.incorrect_letters]))
 
     def one_go(self, letter):
         letter = letter.lower()
@@ -57,36 +70,23 @@ class Hangman():
                     for i in [i for i, j in enumerate(self.word_to_guess) if j == letter]:
                         self.blanks[i] = letter.upper()
                     if "_" in self.blanks:
-                        if self.print_outputs:
-                            print("\n\n\nWell done, \'{}\' is correct!".format(letter.upper()))
-                            self.print_update()
+                        self.print_update(letter, "correct")
                     else:
                         self.is_word_guessed = True
-                        if self.print_outputs:
-                            print("\n\n\nCongratulations, you win!\nThe word was \'{}\'.".format(self.word_to_guess.upper()))
-                            self.print_update()
+                        self.print_update(letter, "won")
                 elif letter not in self.word_to_guess and not self.has_lost_game:
                     self.incorrect_letters.append(letter)
                     if len(self.incorrect_letters) < len(self.pictures)-1:
-                        if self.print_outputs:
-                            print("\n\n\nUnlucky, \'{}\' is incorrect...".format(letter.upper()))
-                            self.print_update()
+                        self.print_update(letter, "incorrect")
                     else:
                         self.has_lost_game = True
-                        if self.print_outputs:
-                            print("\n\n\nUnlucky, \'{}\' is incorrect... and you lose.\nThe word was \'{}\'.".format(letter.upper(), self.word_to_guess.upper()))
-                            self.print_update()
+                        self.print_update(letter, "lost")
                 else:
-                    if self.print_outputs:
-                        print("\n\n\nYou already lost...")
-
-
+                    self.print_update(letter, "already lost", False)
             else:
-                if self.print_outputs:
-                    print("{} has already been tried. Please try another letter.".format(letter))
+                self.print_update(letter, "already tried", False)
         else:
-            if self.print_outputs:
-                print("{} is not a letter. Please try again.".format(letter))
+            self.print_update(letter, "not a letter", False)
 
     def iterate(self, strategy="random"):
         self.letters_left = self.alphabet
@@ -127,7 +127,8 @@ if __name__ == "__main__":
     import_csv = pd.read_csv("dictionary.csv", usecols=[0], header=None, names=["Words"])
     dictionary = np.array(import_csv[~import_csv.Words.str.contains(r'[^a-z]', na=False)]).ravel()
     print("Done.\n")
-    pictures11 = ['\n\n\n\n\n\n\n         ', '\n\n       \n       \n       \n       \n       \n=========', '\n       \n      |\n      |\n      |\n      |\n      |\n=========', '\n  +---+\n      |\n      |\n      |\n      |\n      |\n=========', '\n  +---+\n     \\|\n      |\n      |\n      |\n      |\n=========', '\n  +---+\n  |  \\|\n      |\n      |\n      |\n      |\n=========', '\n  +---+\n  |  \\|\n  O   |\n      |\n      |\n      |\n=========', '\n  +---+\n  |  \\|\n  O   |\n  |   |\n      |\n      |\n=========', '\n  +---+\n  |  \\|\n  O   |\n /|   |\n      |\n      |\n=========', '\n  +---+\n  |  \\|\n  O   |\n /|\\  |\n      |\n      |\n=========', '\n  +---+\n  |  \\|\n  O   |\n /|\\  |\n /    |\n      |\n=========', '\n  +---+\n  |  \\|\n  O   |\n /|\\  |\n / \\  |\n      |\n=========']
+    pictures11_plain = ['\n\n\n\n\n\n\n         ', '\n\n       \n       \n       \n       \n       \n=========', '\n       \n      |\n      |\n      |\n      |\n      |\n=========', '\n  +---+\n      |\n      |\n      |\n      |\n      |\n=========', '\n  +---+\n     \\|\n      |\n      |\n      |\n      |\n=========', '\n  +---+\n  |  \\|\n      |\n      |\n      |\n      |\n=========', '\n  +---+\n  |  \\|\n  O   |\n      |\n      |\n      |\n=========', '\n  +---+\n  |  \\|\n  O   |\n  |   |\n      |\n      |\n=========', '\n  +---+\n  |  \\|\n  O   |\n /|   |\n      |\n      |\n=========', '\n  +---+\n  |  \\|\n  O   |\n /|\\  |\n      |\n      |\n=========', '\n  +---+\n  |  \\|\n  O   |\n /|\\  |\n /    |\n      |\n=========', '\n  +---+\n  |  \\|\n  O   |\n /|\\  |\n / \\  |\n      |\n=========']
+    pictures11 = ['\n\n\n\n\n\n\n         ', '\n\n       \n       \n       \n       \n       \n=========', '\n       \n      |\n      |\n      |\n      |\n      |\n=========', '\n  +---+\n      |\n      |\n      |\n      |\n      |\n=========', '\n  +---+\n     \\|\n      |\n      |\n      |\n      |\n=========', '\n  +---+\n  |  \\|\n      |\n      |\n      |\n      |\n=========', '\n  +---+\n  |  \\|\n  O   |\n      |\n      |\n      |\n=========', '\n  +---+\n  |  \\|\n  O   |\n  |   |\n      |\n      |\n=========', '\n  +---+\n  |  \\|\n  O   |\n /|   |\n      |\n      |\n=========', '\n  +---+\n  |  \\|\n  O   |\n /|\\  |\n      |\n      |\n=========', '\n  +---+\n  |  \\|\n  O   |\n /|\\  |\n /    |\n      |\n=========', '                 ______\n  +---+         / ____/___ _____ ___  ___ \n  |  \|        / / __/ __ `/ __ `__ \/ _ \ \n  O   |       / /_/ / /_/ / / / / / /  __/\n /|\  |       \____/\__,_/_/ /_/ /_/\___/ \n / \  |         / __ \_   _____  _____\n      |        / / / / | / / _ \/ ___/\n=========     / /_/ /| |/ /  __/ /\n              \____/ |___/\___/_/']
     pictures7 = pictures11[5:]
 
     game = Hangman(dictionary)
